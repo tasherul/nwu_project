f0a810280f5f408a099be369235c55b0d18fcc1b
tasherul <47455913+tasherul@users.noreply.github.com>
2019-03-05 21:49:53 +0600

    nav_bar_upload

--- app/src/main/java/com/example/time_parallel/MainActivity.java
+++ app/src/main/java/com/example/time_parallel/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.time_parallel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    btn = (Button) (Button) findViewById(R.id.btnOffline);
+    btn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            openLogin();
+        }
+    });
+
+    }
+    public void openLogin()
+    {
+        Intent in = new Intent(this,dashboard_offline.class);
+        startActivity(in);
     }
 }
