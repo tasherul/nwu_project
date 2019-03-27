12a9ab14ff3ecb3b1801022e8f22a3bfe161394c
Mukto <Mukto.nwu@gmail.com>
2019-03-28 02:35:23 +0600

    new_page_Updated
    Co-Authored-By: Tasherul Islam <tasherul@users.noreply.github.com>
    Co-Authored-By: amitoshkobiraj <amitoshkobiraj@users.noreply.github.com>

---
+++ app/src/main/java/com/example/time_parallel/Main_add_exam.java
@@ -0,0 +1,14 @@
+package com.example.time_parallel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main_add_exam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_add_exam);
+    }
+}
