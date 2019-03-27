0ef4da2e10812dcdfe411a53d1d47cff12c2ad28
tasherul <47455913+tasherul@users.noreply.github.com>
2019-03-28 03:51:42 +0600

    @abdullamaholdar Custom Page

--- app/src/main/java/com/example/time_parallel/dashboard_offline.java
+++ app/src/main/java/com/example/time_parallel/dashboard_offline.java
@@ -84,7 +84,7 @@ public class dashboard_offline extends AppCompatActivity
                 //get the data and append to a list
                 Cursor data = mDatabaseHelper.getData();
                 ArrayList<String> listData = new ArrayList<>();
-                
+
                 while(data.moveToNext()){
                     //get the value from the database in column 1
                     //then add it to the ArrayList
@@ -93,7 +93,7 @@ public class dashboard_offline extends AppCompatActivity
                 }
                 //create the list adapter and set the adapter
                 ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1    , listData);
-                mListView.setAdapter(adapter);
+                //mListView.setAdapter(adapter);
 
 
 
